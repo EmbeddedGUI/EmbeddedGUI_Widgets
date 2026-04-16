f1c45381b5776db071262bfc3d96f8c31c3c2706
wenbo <wenbo13579@163.com>
2026-04-16 17:35:24 +0800

    refactor: finalize button reference widget workflow

--- example/HelloCustomWidgets/input/button/test.c
+++ example/HelloCustomWidgets/input/button/test.c
@@ -130,6 +130,7 @@ static void request_page_snapshot(void)
 
 void test_init_ui(void)
 {
+    ui_ready = 0;
     egui_view_linearlayout_init(EGUI_VIEW_OF(&root_layout));
     egui_view_set_size(EGUI_VIEW_OF(&root_layout), BUTTON_ROOT_WIDTH, BUTTON_ROOT_HEIGHT);
     egui_view_linearlayout_set_orientation(EGUI_VIEW_OF(&root_layout), 0);
