39d46735b709998c7efd58bba62c2545c62ae3e2
wenbo <wenbo13579@163.com>
2026-04-17 02:05:09 +0800

    refactor: finalize annotated_scroll_bar reference widget workflow

--- example/HelloCustomWidgets/navigation/annotated_scroll_bar/test.c
+++ example/HelloCustomWidgets/navigation/annotated_scroll_bar/test.c
@@ -43,6 +43,7 @@ static egui_view_annotated_scroll_bar_t annotated_scroll_bar_compact;
 static egui_view_annotated_scroll_bar_t annotated_scroll_bar_read_only;
 static egui_view_api_t annotated_scroll_bar_compact_api;
 static egui_view_api_t annotated_scroll_bar_read_only_api;
+static uint8_t ui_ready;
 
 EGUI_BACKGROUND_COLOR_PARAM_INIT_ROUND_RECTANGLE(bg_page_panel_param, EGUI_COLOR_HEX(0xF5F7F9), EGUI_ALPHA_100, 14);
 EGUI_BACKGROUND_PARAM_INIT(bg_page_panel_params, &bg_page_panel_param, NULL, NULL);
@@ -98,6 +99,8 @@ static const annotated_scroll_bar_snapshot_t compact_snapshot = {
 static const annotated_scroll_bar_snapshot_t read_only_snapshot = {
         NULL, NULL, read_only_markers, (uint8_t)(sizeof(read_only_markers) / sizeof(read_only_markers[0])), 600, 216, 246, 18, 110};
 
+static void layout_page(void);
+
 static void apply_snapshot(egui_view_t *view, const annotated_scroll_bar_snapshot_t *snapshot)
 {
     egui_view_annotated_scroll_bar_set_title(view, snapshot->title);
@@ -115,6 +118,10 @@ static void apply_primary_snapshot(uint8_t index)
 
     primary_snapshot_index = index % (sizeof(primary_snapshots) / sizeof(primary_snapshots[0]));
     apply_snapshot(EGUI_VIEW_OF(&annotated_scroll_bar_primary), snapshot);
+    if (ui_ready)
+    {
+        layout_page();
+    }
 }
 
 static void apply_primary_default_state(void)
@@ -122,18 +129,54 @@ static void apply_primary_default_state(void)
     apply_primary_snapshot(0);
 }
 
-static void apply_preview_states(void)
+static void apply_compact_state(void)
 {
     apply_snapshot(EGUI_VIEW_OF(&annotated_scroll_bar_compact), &compact_snapshot);
     egui_view_annotated_scroll_bar_set_compact_mode(EGUI_VIEW_OF(&annotated_scroll_bar_compact), 1);
+}
 
+static void apply_read_only_state(void)
+{
     apply_snapshot(EGUI_VIEW_OF(&annotated_scroll_bar_read_only), &read_only_snapshot);
     egui_view_annotated_scroll_bar_set_compact_mode(EGUI_VIEW_OF(&annotated_scroll_bar_read_only), 1);
     egui_view_annotated_scroll_bar_set_read_only_mode(EGUI_VIEW_OF(&annotated_scroll_bar_read_only), 1);
 }
 
+static void apply_preview_states(void)
+{
+    apply_compact_state();
+    apply_read_only_state();
+    if (ui_ready)
+    {
+        layout_page();
+    }
+}
+
+static void layout_local_views(void)
+{
+    egui_view_linearlayout_layout_childs(EGUI_VIEW_OF(&bottom_row));
+    egui_view_linearlayout_layout_childs(EGUI_VIEW_OF(&root_layout));
+}
+
+static void layout_page(void)
+{
+    layout_local_views();
+    egui_core_layout_childs_user_root_view(EGUI_LAYOUT_VERTICAL, EGUI_ALIGN_HCENTER | EGUI_ALIGN_VCENTER);
+}
+
+#if EGUI_CONFIG_RECORDING_TEST
+static void request_page_snapshot(void)
+{
+    layout_page();
+    egui_view_invalidate(EGUI_VIEW_OF(&root_layout));
+    recording_request_snapshot();
+}
+#endif
+
 void test_init_ui(void)
 {
+    ui_ready = 0;
+
     egui_view_linearlayout_init(EGUI_VIEW_OF(&root_layout));
     egui_view_set_size(EGUI_VIEW_OF(&root_layout), ANNOTATED_SCROLL_BAR_ROOT_WIDTH, ANNOTATED_SCROLL_BAR_ROOT_HEIGHT);
     egui_view_linearlayout_set_orientation(EGUI_VIEW_OF(&root_layout), 0);
@@ -199,11 +242,11 @@ void test_init_ui(void)
         hello_custom_widgets_demo_apply_title_only_scaffold(EGUI_VIEW_OF(&root_layout), EGUI_VIEW_OF(&title_label), NULL, 0);
     }
 
-    egui_view_linearlayout_layout_childs(EGUI_VIEW_OF(&bottom_row));
-    egui_view_linearlayout_layout_childs(EGUI_VIEW_OF(&root_layout));
-
+    layout_local_views();
     egui_core_add_user_root_view(EGUI_VIEW_OF(&root_layout));
-    egui_core_layout_childs_user_root_view(EGUI_LAYOUT_VERTICAL, EGUI_ALIGN_HCENTER | EGUI_ALIGN_VCENTER);
+    ui_ready = 1;
+    apply_primary_default_state();
+    apply_preview_states();
 }
 
 #if EGUI_CONFIG_RECORDING_TEST
@@ -233,7 +276,7 @@ bool egui_port_get_recording_action(int action_index, egui_sim_action_t *p_actio
         {
             apply_primary_default_state();
             apply_preview_states();
-            recording_request_snapshot();
+            request_page_snapshot();
         }
         EGUI_SIM_SET_WAIT(p_action, ANNOTATED_SCROLL_BAR_RECORD_FRAME_WAIT);
         return true;
@@ -247,7 +290,7 @@ bool egui_port_get_recording_action(int action_index, egui_sim_action_t *p_actio
     case 2:
         if (first_call)
         {
-            recording_request_snapshot();
+            request_page_snapshot();
         }
         EGUI_SIM_SET_WAIT(p_action, ANNOTATED_SCROLL_BAR_RECORD_FRAME_WAIT);
         return true;
@@ -261,7 +304,7 @@ bool egui_port_get_recording_action(int action_index, egui_sim_action_t *p_actio
     case 4:
         if (first_call)
         {
-            recording_request_snapshot();
+            request_page_snapshot();
         }
         EGUI_SIM_SET_WAIT(p_action, ANNOTATED_SCROLL_BAR_RECORD_FRAME_WAIT);
         return true;
@@ -275,7 +318,7 @@ bool egui_port_get_recording_action(int action_index, egui_sim_action_t *p_actio
     case 6:
         if (first_call)
         {
-            recording_request_snapshot();
+            request_page_snapshot();
         }
         EGUI_SIM_SET_WAIT(p_action, ANNOTATED_SCROLL_BAR_RECORD_FRAME_WAIT);
         return true;
@@ -289,7 +332,7 @@ bool egui_port_get_recording_action(int action_index, egui_sim_action_t *p_actio
     case 8:
         if (first_call)
         {
-            recording_request_snapshot();
+            request_page_snapshot();
         }
         EGUI_SIM_SET_WAIT(p_action, ANNOTATED_SCROLL_BAR_RECORD_FRAME_WAIT);
         return true;
@@ -304,7 +347,7 @@ bool egui_port_get_recording_action(int action_index, egui_sim_action_t *p_actio
     case 10:
         if (first_call)
         {
-            recording_request_snapshot();
+            request_page_snapshot();
         }
         EGUI_SIM_SET_WAIT(p_action, ANNOTATED_SCROLL_BAR_RECORD_FINAL_WAIT);
         return true;
