7c152e83e51e3e523be2aeb418ac188c8f4fc0ca
wenbo <wenbo13579@163.com>
2026-04-21 16:19:06 +0800

    1. keyboard关联

--- example/HelloCustomWidgets/input/search_box/test.c
+++ example/HelloCustomWidgets/input/search_box/test.c
@@ -21,6 +21,9 @@
 #define SEARCH_BOX_RECORD_FRAME_WAIT 170
 #define SEARCH_BOX_RECORD_FINAL_WAIT 280
 #define SEARCH_BOX_DEFAULT_SNAPSHOT  0
+#define SEARCH_BOX_KEYBOARD_HEIGHT   128
+#define SEARCH_BOX_KEYBOARD_Y        ((EGUI_CONFIG_SCEEN_HEIGHT > SEARCH_BOX_KEYBOARD_HEIGHT) ? (EGUI_CONFIG_SCEEN_HEIGHT - SEARCH_BOX_KEYBOARD_HEIGHT) : 0)
+#define SEARCH_BOX_KEYBOARD_HIDDEN_Y (EGUI_CONFIG_SCEEN_HEIGHT + SEARCH_BOX_KEYBOARD_HEIGHT)
 
 #define PRIMARY_SNAPSHOT_COUNT ((uint8_t)EGUI_ARRAY_SIZE(primary_snapshots))
 
@@ -40,6 +43,10 @@ static egui_view_search_box_t box_compact;
 static egui_view_search_box_t box_read_only;
 static egui_view_api_t box_compact_api;
 static egui_view_api_t box_read_only_api;
+#if EGUI_CONFIG_FUNCTION_SUPPORT_KEY && EGUI_CONFIG_FUNCTION_SUPPORT_FOCUS
+static egui_view_keyboard_t box_keyboard;
+static egui_view_api_t box_primary_focus_api;
+#endif
 static uint8_t ui_ready;
 
 EGUI_BACKGROUND_COLOR_PARAM_INIT_ROUND_RECTANGLE(bg_page_panel_param, EGUI_COLOR_HEX(0xF5F7F9), EGUI_ALPHA_100, 14);
@@ -58,6 +65,33 @@ static const search_box_snapshot_t compact_snapshot = {"Recent", "Assets", 16};
 static const search_box_snapshot_t read_only_snapshot = {"Pinned", "Archive", 16};
 
 static void layout_page(void);
+static void relocate_view(egui_view_t *view, egui_dim_t x, egui_dim_t y);
+
+#if EGUI_CONFIG_FUNCTION_SUPPORT_FOCUS
+static void clear_primary_focus(egui_view_t *self)
+{
+    EGUI_UNUSED(self);
+    egui_view_clear_focus(EGUI_VIEW_OF(&box_primary));
+}
+#endif
+
+#if EGUI_CONFIG_FUNCTION_SUPPORT_KEY && EGUI_CONFIG_FUNCTION_SUPPORT_FOCUS
+static void position_keyboard_visible(void)
+{
+    relocate_view(EGUI_VIEW_OF(&box_keyboard), 0, SEARCH_BOX_KEYBOARD_Y);
+}
+
+static void position_keyboard_hidden(void)
+{
+    relocate_view(EGUI_VIEW_OF(&box_keyboard), 0, SEARCH_BOX_KEYBOARD_HIDDEN_Y);
+}
+
+static void on_primary_submit(egui_view_t *self, const char *text)
+{
+    EGUI_UNUSED(text);
+    egui_view_clear_focus(self);
+}
+#endif
 
 static void apply_snapshot(egui_view_t *view, const search_box_snapshot_t *snapshot)
 {
@@ -100,18 +134,124 @@ static void layout_local_views(void)
     egui_view_linearlayout_layout_childs(EGUI_VIEW_OF(&root_layout));
 }
 
+static void relocate_view(egui_view_t *view, egui_dim_t x, egui_dim_t y)
+{
+    egui_region_t region;
+
+    if (view->region.location.x == x && view->region.location.y == y)
+    {
+        return;
+    }
+
+    region = view->region;
+    region.location.x = x;
+    region.location.y = y;
+    egui_view_layout(view, &region);
+}
+
+static egui_dim_t get_stable_root_y(void)
+{
+    egui_dim_t root_y = (EGUI_CONFIG_SCEEN_HEIGHT - EGUI_VIEW_OF(&root_layout)->region.size.height) / 2;
+
+#if EGUI_CONFIG_FUNCTION_SUPPORT_KEY && EGUI_CONFIG_FUNCTION_SUPPORT_FOCUS
+    if (EGUI_VIEW_OF(&box_keyboard)->region.size.height > 0)
+    {
+        egui_dim_t max_root_bottom = EGUI_CONFIG_SCEEN_HEIGHT - EGUI_VIEW_OF(&box_keyboard)->region.size.height - 12;
+
+        if (root_y + EGUI_VIEW_OF(&root_layout)->region.size.height > max_root_bottom)
+        {
+            root_y = max_root_bottom - EGUI_VIEW_OF(&root_layout)->region.size.height;
+        }
+    }
+#endif
+    if (root_y < 0)
+    {
+        root_y = 0;
+    }
+    return root_y;
+}
+
 static void layout_page(void)
 {
+    egui_dim_t root_x = (EGUI_CONFIG_SCEEN_WIDTH - EGUI_VIEW_OF(&root_layout)->region.size.width) / 2;
+
     layout_local_views();
-    egui_core_layout_childs_user_root_view(uicode_get_core(), EGUI_LAYOUT_VERTICAL, EGUI_ALIGN_HCENTER | EGUI_ALIGN_VCENTER);
+    if (root_x < 0)
+    {
+        root_x = 0;
+    }
+    relocate_view(EGUI_VIEW_OF(&root_layout), root_x, get_stable_root_y());
 }
+
+#if EGUI_CONFIG_FUNCTION_SUPPORT_KEY && EGUI_CONFIG_FUNCTION_SUPPORT_FOCUS
+static void on_primary_focus_changed(egui_view_t *self, int is_focused)
+{
+    egui_view_textinput_t *textinput = &((egui_view_search_box_t *)self)->textinput;
+
+    if (is_focused)
+    {
+        textinput->cursor_visible = 1;
+        egui_view_start_timer(self, &textinput->cursor_timer, EGUI_CONFIG_TEXTINPUT_CURSOR_BLINK_MS, 0);
+        position_keyboard_visible();
+        egui_view_keyboard_show(EGUI_VIEW_OF(&box_keyboard), self);
+    }
+    else
+    {
+        textinput->cursor_visible = 0;
+        egui_view_stop_timer(self, &textinput->cursor_timer);
+        egui_view_keyboard_hide(EGUI_VIEW_OF(&box_keyboard));
+        position_keyboard_hidden();
+    }
+    if (ui_ready)
+    {
+        layout_page();
+        egui_core_update_region_dirty_all(uicode_get_core());
+    }
+    egui_view_invalidate(self);
+}
+#endif
 
 #if EGUI_CONFIG_RECORDING_TEST
 static void request_page_snapshot(void)
 {
     layout_page();
-    egui_view_invalidate(EGUI_VIEW_OF(&root_layout));
+    egui_core_update_region_dirty_all(uicode_get_core());
     recording_request_snapshot();
+}
+
+static uint8_t set_primary_field_click_action(egui_sim_action_t *p_action, int interval_ms)
+{
+    egui_view_t *view = EGUI_VIEW_OF(&box_primary);
+
+    if (p_action == NULL || view->region_screen.size.width <= 0 || view->region_screen.size.height <= 0)
+    {
+        return 0;
+    }
+
+    p_action->type = EGUI_SIM_ACTION_CLICK;
+    p_action->x1 = view->region_screen.location.x + view->region_screen.size.width / 2;
+    p_action->y1 = view->region_screen.location.y + view->region_screen.size.height / 2;
+    p_action->interval_ms = interval_ms;
+    return 1;
+}
+
+static void apply_primary_key(uint8_t key_code)
+{
+    egui_key_event_t event = {0};
+
+#if EGUI_CONFIG_FUNCTION_SUPPORT_FOCUS
+    egui_view_request_focus(EGUI_VIEW_OF(&box_primary));
+#endif
+    event.type = EGUI_KEY_EVENT_ACTION_DOWN;
+    event.key_code = key_code;
+    egui_view_dispatch_key_event(EGUI_VIEW_OF(&box_primary), &event);
+
+    event.type = EGUI_KEY_EVENT_ACTION_UP;
+    egui_view_dispatch_key_event(EGUI_VIEW_OF(&box_primary), &event);
+    if (ui_ready)
+    {
+        layout_page();
+    }
 }
 #endif
 
@@ -124,6 +264,9 @@ void test_init_ui(void)
     egui_view_linearlayout_set_orientation(EGUI_VIEW_OF(&root_layout), 0);
     egui_view_linearlayout_set_align_type(EGUI_VIEW_OF(&root_layout), EGUI_ALIGN_HCENTER);
     egui_view_set_background(EGUI_VIEW_OF(&root_layout), EGUI_BG_OF(&bg_page_panel));
+#if EGUI_CONFIG_FUNCTION_SUPPORT_FOCUS
+    egui_view_set_on_click_listener(EGUI_VIEW_OF(&root_layout), clear_primary_focus);
+#endif
 
     egui_view_label_init(EGUI_VIEW_OF(&title_label), uicode_get_core());
     egui_view_set_size(EGUI_VIEW_OF(&title_label), SEARCH_BOX_ROOT_WIDTH, 18);
@@ -141,6 +284,9 @@ void test_init_ui(void)
     egui_view_set_margin(EGUI_VIEW_OF(&box_primary), 0, 0, 0, 8);
 #if EGUI_CONFIG_FUNCTION_SUPPORT_FOCUS
     egui_view_set_focusable(EGUI_VIEW_OF(&box_primary), true);
+#endif
+#if EGUI_CONFIG_FUNCTION_SUPPORT_KEY && EGUI_CONFIG_FUNCTION_SUPPORT_FOCUS
+    egui_view_textinput_set_on_submit(EGUI_VIEW_OF(&box_primary), on_primary_submit);
 #endif
     egui_view_group_add_child(EGUI_VIEW_OF(&root_layout), EGUI_VIEW_OF(&box_primary));
 
@@ -178,6 +324,20 @@ void test_init_ui(void)
 
     layout_local_views();
     egui_core_add_user_root_view(EGUI_VIEW_OF(&root_layout));
+#if EGUI_CONFIG_FUNCTION_SUPPORT_KEY && EGUI_CONFIG_FUNCTION_SUPPORT_FOCUS
+    egui_view_keyboard_init(EGUI_VIEW_OF(&box_keyboard), uicode_get_core());
+#if EGUI_CONFIG_FUNCTION_SUPPORT_LAYER
+    egui_view_set_layer(EGUI_VIEW_OF(&box_keyboard), EGUI_VIEW_LAYER_TOP);
+#endif
+    egui_view_set_position(EGUI_VIEW_OF(&box_keyboard), 0, SEARCH_BOX_KEYBOARD_HIDDEN_Y);
+    egui_view_set_size(EGUI_VIEW_OF(&box_keyboard), EGUI_CONFIG_SCEEN_WIDTH, SEARCH_BOX_KEYBOARD_HEIGHT);
+    position_keyboard_hidden();
+    egui_view_keyboard_set_font(EGUI_VIEW_OF(&box_keyboard), (const egui_font_t *)EGUI_CONFIG_FONT_DEFAULT);
+    egui_view_keyboard_set_icon_font(EGUI_VIEW_OF(&box_keyboard), EGUI_FONT_ICON_MS_20);
+    egui_view_keyboard_set_special_key_icons(EGUI_VIEW_OF(&box_keyboard), EGUI_ICON_MS_KEYBOARD_ARROW_UP, EGUI_ICON_MS_BACKSPACE, EGUI_ICON_MS_DONE);
+    egui_core_add_user_root_view(EGUI_VIEW_OF(&box_keyboard));
+    egui_view_override_api_on_focus_changed(EGUI_VIEW_OF(&box_primary), &box_primary_focus_api, on_primary_focus_changed);
+#endif
     ui_ready = 1;
     apply_primary_default_state();
     apply_preview_states();
@@ -203,11 +363,10 @@ bool egui_port_get_recording_action(int action_index, egui_sim_action_t *p_actio
         EGUI_SIM_SET_WAIT(p_action, SEARCH_BOX_RECORD_FRAME_WAIT);
         return true;
     case 1:
-        if (first_call)
+        if (!set_primary_field_click_action(p_action, SEARCH_BOX_RECORD_WAIT))
         {
-            apply_primary_snapshot(1);
+            EGUI_SIM_SET_WAIT(p_action, SEARCH_BOX_RECORD_WAIT);
         }
-        EGUI_SIM_SET_WAIT(p_action, SEARCH_BOX_RECORD_WAIT);
         return true;
     case 2:
         if (first_call)
@@ -219,7 +378,7 @@ bool egui_port_get_recording_action(int action_index, egui_sim_action_t *p_actio
     case 3:
         if (first_call)
         {
-            apply_primary_snapshot(2);
+            apply_primary_key(EGUI_KEY_CODE_BACKSPACE);
         }
         EGUI_SIM_SET_WAIT(p_action, SEARCH_BOX_RECORD_WAIT);
         return true;
@@ -233,7 +392,7 @@ bool egui_port_get_recording_action(int action_index, egui_sim_action_t *p_actio
     case 5:
         if (first_call)
         {
-            apply_primary_default_state();
+            apply_primary_key(EGUI_KEY_CODE_ENTER);
         }
         EGUI_SIM_SET_WAIT(p_action, SEARCH_BOX_RECORD_WAIT);
         return true;
@@ -249,4 +408,3 @@ bool egui_port_get_recording_action(int action_index, egui_sim_action_t *p_actio
     }
 }
 #endif
-
